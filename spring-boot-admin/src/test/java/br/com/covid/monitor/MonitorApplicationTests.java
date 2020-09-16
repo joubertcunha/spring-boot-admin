e14158b62e46abf59d65e53c0258bdc5e7b1b463
Joubert Cunha <joubertgoncalves@gmail.com>
2020-09-16 18:39:59 -0300

    Projeto com o intuito de exibir as informações do monitoramento da api-dados-covid

---
+++ spring-boot-admin/src/test/java/br/com/covid/monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.covid.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
